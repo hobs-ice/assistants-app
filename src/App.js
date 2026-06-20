4131d8f2751ea91300ffe84ff723b692e0e1828d
Ali Assoumani <aliassoumani@macbook-air-de-ali.home>
2026-06-20 12:22:24 +0400

    webhook anon

--- src/App.js
+++ src/App.js
@@ -84,7 +84,9 @@ function Assistant({ id, onBack, hasAccess }) {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${currentSession?.access_token}`,
+      'Authorization': `Bearer ${currentSession?.access_token || ''}`,
+'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3dG5nZG12bGZnb3B0d2RlamplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NjIyMzksImV4cCI6MjA5NzQzODIzOX0.2wFMBxHWhNinbsJd_dZ4W4SR7btKlJhJSe-x__5Q1aY',
+
     },
     body: JSON.stringify({
       priceId: 'price_1TkJOgCdR1GrgAhcS0fd5gzc',
