f291e48e0305989916a38208ffdc8d1f87c817d5
Ali Assoumani <aliassoumani@macbook-air-de-ali.home>
2026-06-20 12:49:57 +0400

    Debug session token3

--- src/App.js
+++ src/App.js
@@ -87,8 +87,7 @@ console.log('Session token:', currentSession?.access_token);
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${currentSession?.access_token || ''}`,
-'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3dG5nZG12bGZnb3B0d2RlamplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NjIyMzksImV4cCI6MjA5NzQzODIzOX0.2wFMBxHWhNinbsJd_dZ4W4SR7btKlJhJSe-x__5Q1aY',
+      'Authorization': `Bearer ${currentSession?.access_token}`,
 
     },
     body: JSON.stringify({
